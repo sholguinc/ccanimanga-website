e7cf6f914df4d4c7a54a6748510533df08aa1e4c
Sergio Holguin <sholguinc@uni.pe>
2023-09-19 14:13:38 -0500

    first commmit - init project

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import * as path from 'path';
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "./src/styles/globals.scss";',
+      },
+    },
+  },
+})
