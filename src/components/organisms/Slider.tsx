5f56fe5c6edb2166956c9a87cd0da0263c8274f1
Sergio Holguin <sholguinc@uni.pe>
2023-10-16 22:01:36 -0500

    added autoplay in slider

--- src/components/organisms/Slider.tsx
+++ src/components/organisms/Slider.tsx
@@ -1,4 +1,4 @@
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -8,12 +8,13 @@ import { ISlider } from '@/models';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
 
 export const Slider = ({ slides }: ISlider) => {
   return (
     <div className="slider">
       <Swiper
-        modules={[Navigation, Pagination]}
+        modules={[Navigation, Pagination, Autoplay]}
         navigation={{
           prevEl: '.slider__angle-left',
           nextEl: '.slider__angle-right',
@@ -24,9 +25,14 @@ export const Slider = ({ slides }: ISlider) => {
           renderBullet: (_, className) =>
             '<span class="' + className + '">' + '' + '</span>',
         }}
-        speed={600}
+        speed={500}
         slidesPerView={1}
+        autoplay={{
+          delay: 8000,
+          disableOnInteraction: false,
+        }}
         loop={true}
+        allowTouchMove={false}
         className="slider__swiper"
       >
         {slides.map((slide, index) => (
