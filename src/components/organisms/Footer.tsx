b32b1eb044e6bbb4bee0cf68425be2ddd8a59f2c
Sergio Holguin <sholguinc@uni.pe>
2023-10-27 01:53:33 -0500

    footer completed

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -6,23 +6,23 @@ export const Footer = () => {
     <footer className="footer">
       <div className="footer__content">
         <div className="footer__text">
+          <div className="footer__social-media">
+            <Text
+              size="larger"
+              color="ccam-red"
+              weight="bold-medium"
+              className="footer__follow-us"
+            >
+              Síguenos:
+            </Text>
+            <SocialMediaList invert={true} gap={10} />
+          </div>
           <OrganizationList />
           <Text weight="light" family="noto-jp" className="footer__copyright">
             &copy; 2023 • Sergio Holguín • Lima, Perú
           </Text>
         </div>
         <AngleUp />
-        <div className="footer__social-media">
-          <Text
-            size="larger"
-            color="ccam-red"
-            weight="bold-medium"
-            className="footer__follow-us"
-          >
-            Síguenos:
-          </Text>
-          <SocialMediaList invert={true} gap={10} />
-        </div>
       </div>
     </footer>
   );
