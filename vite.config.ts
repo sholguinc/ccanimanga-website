9e8e27fcb858f55c9d1390debc7ba81c16ebb5db
Sergio Holguin <sholguinc@uni.pe>
2023-12-01 23:05:24 -0500

    added vite image optimizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import * as path from 'path';
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from "vite-plugin-svgr";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [
+    ViteImageOptimizer({
+      // Include only SVG files
+      include: ['**/*.svg'],
+      // Exclude all other file types
+      exclude: ['**/*.{png,jpg,jpeg,gif,webp,avif}'],
+    }),
+    svgr(),
+    react(),
+  ],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
@@ -17,4 +27,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
